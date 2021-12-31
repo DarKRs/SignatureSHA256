408d2323b6eabc3fca633b5bb62009ad55a08064
DarKRs <centralstreet_24@mail.ru>
2021-12-31 12:37:32 +0600

    Signature

--- SignatureSHA256/Program.cs
+++ SignatureSHA256/Program.cs
@@ -1,12 +1,53 @@
 ﻿using System;
+using System.Threading;
 
 namespace SignatureSHA256
 {
     class Program
     {
-        static void Main(string[] args)
+
+        static void  Main()
         {
-            Console.WriteLine("Hello World!");
+            int nWorkerThreads;
+            int nCompletionThreads;
+            ThreadPool.GetMaxThreads(out nWorkerThreads, out nCompletionThreads);
+            Console.WriteLine("Максимальное количество потоков: " + nWorkerThreads
+                + "\nПотоков ввода-вывода доступно: " + nCompletionThreads);
+            Console.WriteLine("Кол-во процессоров на компьютере {0}.", Environment.ProcessorCount);
+
+
+            Console.WriteLine("Введите путь до файла для которого требуется создать сигнатуру:");
+            string path = Console.ReadLine();
+            Console.WriteLine("Введите на блоки какой длины требуется разделить файл:");
+            int blockSize = Convert.ToInt32(Console.ReadLine());
+
+            var rd = new Reader(path, blockSize);
+             for(int i=0; i < Environment.ProcessorCount+1; i++)
+             {
+                new Thread(() => { rd.Read(); }).Start();
+                //Thread.Sleep(100);
+             }
+            while (!rd.Closed)
+            {
+                try
+                {
+                    Thread.Sleep(100);
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine("\n Исключение в потоке: " + Thread.CurrentThread.GetHashCode().ToString());
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine(new System.Diagnostics.StackTrace().ToString());
+                    Console.ReadKey();
+                }
+            }
+            rd.Close();
+
+            Console.ReadLine();
         }
+
     }
+
+
+
 }
---
+++ SignatureSHA256/Reader.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Security.Cryptography;
+using System.Diagnostics;
+
+namespace SignatureSHA256
+{
+    class Reader
+    {
+
+        #region Приватные свойства
+
+        private Stopwatch myStopwatch = new System.Diagnostics.Stopwatch();
+        private  readonly object Lock = new object();
+        private long offset = 0;
+        private readonly int blockSize;
+        private byte[] buffer;
+        private byte[] byteArray;
+        private StringBuilder sb = new StringBuilder();
+        private int blockNum=0;
+        private readonly long LenghtFile = 0;
+        private SHA256 sha = SHA256.Create();
+        private FileStream fs;
+
+        #endregion
+
+        #region Публичные свойства
+
+        public bool Closed = false;
+
+        #endregion
+
+        #region Конструктор
+
+        public Reader(string _path, int _blockSize)
+        {
+            blockSize = _blockSize;
+            buffer = new byte[_blockSize];
+            fs = File.OpenRead(_path);
+            myStopwatch.Start();
+            Closed = false;
+            LenghtFile = fs.Length;
+        }
+
+        #endregion
+
+        #region Методы
+
+        /// <summary>
+        /// Основной метод для работы с файлом и расчётом хэша из блоков. Метод было решено объединить в один, т.к.
+        /// при расчёте в одном методе увеличивалась скорость обработки.
+        /// </summary>
+        public void Read()
+        {
+            while (offset < LenghtFile - 2*blockSize && !Closed) 
+                {
+                Monitor.Enter(Lock);
+                try
+                {
+                    if (Closed || offset > LenghtFile - blockSize)
+                        break;
+                    buffer = new byte[blockSize];
+                    fs.Seek(offset, SeekOrigin.Begin);
+                    fs.Read(buffer, 0, blockSize);
+
+
+                    byteArray = sha.ComputeHash(buffer);
+                    sb.Clear();
+                    foreach (byte a in byteArray)
+                    {
+                        //Преобразование a в шестнадцатеричный формат
+                        sb.Append(a.ToString("x2"));
+                    }
+                    Interlocked.Increment(ref blockNum);
+
+                    Console.WriteLine("\nБлок №" + blockNum + " : " + sb);
+                    Interlocked.Add(ref offset, blockSize);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("\n Исключение в потоке: " + Thread.CurrentThread.GetHashCode().ToString());
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine(new System.Diagnostics.StackTrace().ToString());
+                    Console.ReadKey();
+                }
+                finally
+                {
+                    Monitor.Exit(Lock);
+                }
+            }
+            Closed = true;
+            myStopwatch.Stop();
+        }
+
+        /// <summary>
+        /// Побочный метод для закрытия FileStream и вывода информации на экран
+        /// </summary>
+        public void Close()
+        {
+            fs.Close();
+            Console.WriteLine("\nКонец файла. Всего блоков - " + blockNum + "(" + (blockNum + 1) + ")");
+            Console.WriteLine("\nВремя выполнения - " + myStopwatch.ElapsedMilliseconds.ToString());
+        }
+
+        #endregion
+
+    }
+}
